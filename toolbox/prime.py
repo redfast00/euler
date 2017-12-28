4dfd493902e322985499097b3e3aa140511fc150
redfast00 <redfast00@gmail.com>
2017-12-28 19:03:01 +0100

    Added is_prime

--- toolbox/prime.py
+++ toolbox/prime.py
@@ -12,3 +12,9 @@ def divisors(number):
     for factor in factorize(number):
         found.update({x*factor for x in found})
     return found
+
+def is_prime(number):
+    for i in range(2, int(math.sqrt(number)+1)):
+        if number % i == 0:
+            return False
+    return True
