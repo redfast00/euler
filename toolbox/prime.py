65a3f6688a722829bb475a2d585537c87407ab60
redfast00 <redfast00@gmail.com>
2017-12-30 11:29:46 +0100

    All primes below

--- toolbox/prime.py
+++ toolbox/prime.py
@@ -18,3 +18,20 @@ def is_prime(number):
         if number % i == 0:
             return False
     return True
+
+def all_primes_below(number):
+    assert number >= 2
+    primes = []
+    candidate = 2
+    while candidate < number:
+        breakpoint = int(math.sqrt(candidate))
+        for prime in primes:
+            if candidate % prime == 0:
+                break
+            if prime > breakpoint:
+                primes.append(candidate)
+                break
+        else:
+            primes.append(candidate)
+        candidate += 1
+    return primes
