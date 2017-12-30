49dbb53533d2ccfac32a5f6d921df18660ea695d
redfast00 <redfast00@gmail.com>
2017-12-30 14:50:17 +0100

    Improved is_prime

--- toolbox/prime.py
+++ toolbox/prime.py
@@ -14,6 +14,8 @@ def divisors(number):
     return found
 
 def is_prime(number):
+    if number < 2:
+        return False
     for i in range(2, int(math.sqrt(number)+1)):
         if number % i == 0:
             return False
