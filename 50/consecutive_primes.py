55785449d42b79537560b754c42fac49b3752925
redfast00 <redfast00@gmail.com>
2018-01-13 19:57:13 +0100

    Improved performance

--- 50/consecutive_primes.py
+++ 50/consecutive_primes.py
@@ -1,4 +1,4 @@
-from toolbox import stream_primes, is_prime, all_primes_below
+from toolbox import all_primes_below
 
 def longest_prime_below(upperbound):
     chain_length = 2
@@ -7,22 +7,33 @@ def longest_prime_below(upperbound):
     max_ = 0
     tried_at_least_one = True
     while tried_at_least_one:
-        tried_at_least_one = False
-        for cur_idx in range(0, len(primes) - chain_length):
-            candidate = sum(primes[cur_idx:cur_idx+chain_length])
-            if candidate >= upperbound:
-                # exceeded maximum, increase chain length and restart
-                chain_length += 1
-                break
-            else:
-                tried_at_least_one = True
+        if chain_length % 2 == 0:
+            # we have an even number of primes, so we should only consider a sequence
+            #  starting with 2 ... or the sum of the sequence will be even and so not a prime
+            candidate = sum(primes[:chain_length])
             if candidate in primeset:
                 # Found a prime! set the current maximum to this prime,
                 # increase chain lenght and restart
                 max_ = candidate
                 print(f"Found {max_} at chain lenght {chain_length}")
-                chain_length += 1
-                break
+            chain_length += 1
+        else:
+            tried_at_least_one = False
+            for cur_idx in range(0, len(primes) - chain_length):
+                candidate = sum(primes[cur_idx:cur_idx+chain_length])
+                if candidate >= upperbound:
+                    # exceeded maximum, increase chain length and restart
+                    chain_length += 1
+                    break
+                else:
+                    tried_at_least_one = True
+                if candidate in primeset:
+                    # Found a prime! set the current maximum to this prime,
+                    # increase chain lenght and restart
+                    max_ = candidate
+                    print(f"Found {max_} at chain lenght {chain_length}")
+                    chain_length += 1
+                    break
     return max_
 
 print(longest_prime_below(1000*1000))
