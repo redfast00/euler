03748c62ebe02246670e2636b1d8a223e1cd1e3f
redfast00 <redfast00@gmail.com>
2018-01-14 22:08:56 +0100

    DynamicPrime: on the fly generation of primes

--- toolbox/prime.py
+++ toolbox/prime.py
@@ -1,4 +1,6 @@
 import math
+import itertools
+import bisect
 
 def factorize(number):
     for i in range(2, int(math.sqrt(number)+1)):
@@ -54,3 +56,43 @@ def stream_primes():
             primes.append(candidate)
             yield candidate
         candidate += 1
+
+class DynamicPrime(object):
+    def __init__(self):
+        self.primes = [2, 3]
+
+    def __iter__(self):
+        for i in itertools.count(0):
+            yield self[i]
+
+    def __getitem__(self, key):
+        while len(self.primes) <= key:
+            self._generate_next()
+
+        return self.primes[key]
+
+    def is_prime(self, number):
+        assert number > 1
+        if number > self.primes[-1]:
+            while number > self.primes[-1]:
+                self._generate_next()
+            return number == self.primes[-1]
+        else:
+            # Lookup prime with binary search in already generated primes
+            place = bisect.bisect_left(self.primes, number)
+            return (place != len(self.primes) and self.primes[place] == number)
+
+    def _generate_next(self):
+        candidate = self.primes[-1] + 1
+        while True:
+            breakpoint = int(math.sqrt(candidate))
+            for prime in self.primes:
+                if candidate % prime == 0:
+                    break
+                if prime > breakpoint:
+                    self.primes.append(candidate)
+                    return candidate
+            else:
+                self.primes.append(candidate)
+                return candidate
+            candidate += 1
